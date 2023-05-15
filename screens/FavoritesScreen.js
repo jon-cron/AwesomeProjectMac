b7124c08702b4c4b486d7f13f408003dd5d696a5
jon-cron <jonathanacm95@gmail.com>
2023-05-15 10:21:47 -0600

    app is finished

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,24 +1,27 @@
-import React, { useContext } from 'react';
-import {View, Text, Image, Pressable, StyleSheet} from 'react-native';
-import MealsList from '../components/MealsList';
-import { FavoriteContext } from '../store/context/favorites-context';
-import { MEALS } from '../data/dummbyData';
-
+import React, { useContext } from "react";
+import { View, Text, Image, Pressable, StyleSheet } from "react-native";
+import MealsList from "../components/MealsList";
+import { FavoriteContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummbyData";
+import { useSelector } from "react-redux";
 
 const FavoritesScreen = () => {
-const favContext = useContext(FavoriteContext)
-// NOTE this is an excellent way to check to see if an id is in two different arrays
-const favoriteMeals = MEALS.filter(meal => favContext.ids.includes(meal.id))
-if(favoriteMeals.length === 0) {
+  //   const favContext = useContext(FavoriteContext);
+  // NOTE this is an excellent way to check to see if an id is in two different arrays
+  const favoriteMealsIds = useSelector((state) => state.favoriteMeals.ids);
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsIds.includes(meal.id)
+  );
+  if (favoriteMeals.length === 0) {
     return (
-        <View><Text>There are no favorites</Text></View>
-    )
-}
-return (
-<MealsList items={favoriteMeals}/>
-)
-}
+      <View>
+        <Text>There are no favorites</Text>
+      </View>
+    );
+  }
+  return <MealsList items={favoriteMeals} />;
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 // NOTE will finish this app on somedat
-export default FavoritesScreen
+export default FavoritesScreen;
