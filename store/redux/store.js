2f4d1b6ae6e52b628973b6b3077896e755ce9321
jon-cron <jonathanacm95@gmail.com>
2023-05-15 09:41:55 -0600

    finishing app today

---
+++ store/redux/store.js
@@ -0,0 +1,6 @@
+// NOTE to begin using reduce max recommends using the redux toolkit. To use this run 'npm install @reduxjs/toolkit react-redux'
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer: {}
+})
