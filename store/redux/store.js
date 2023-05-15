043e5a9704d221c14c92aa590911c5a0c1940af3
jon-cron <jonathanacm95@gmail.com>
2023-05-15 10:03:45 -0600

    using slice

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,6 +1,8 @@
 // NOTE to begin using reduce max recommends using the redux toolkit. To use this run 'npm install @reduxjs/toolkit react-redux'
 import { configureStore } from "@reduxjs/toolkit";
-
+import favoritesReducer from "./favorites";
 export const store = configureStore({
-    reducer: {}
-})
+  reducer: {
+    favoriteMeals: favoritesReducer,
+  },
+});
